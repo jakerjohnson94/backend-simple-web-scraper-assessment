a36258115c0e35849e7a66e63696ddf97dde30f3
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-12-03 12:38:31 -0500

    fixed parser to scrape urls from link and img tags and scrape phone numbers from links

--- scraper.py
+++ scraper.py
@@ -8,32 +8,38 @@ __author__ = 'Jake Johnson'
 
 
 class HTML_Info_Parser(HTMLParser):
-    phone_list, email_list, url_list = [], [], []
+    def __init__(self):
+        HTMLParser.__init__(self)
 
-    # ensure proper formatting and remove whitespace
-    def format_data(self, *strings):
-        for string in strings:
-            string = str(string.encode('ascii')).strip()
-        return strings
-
-    # sort list and remove duplicates
-    def format_list(self, lst):
-        return sorted(list(set(lst)))
-
-    def handle_data(self, data):
-        """ find regex matches and format data for output """
-        format_list, format_data = self.format_list, self.format_data
-
-        # ensure images do not get scraped as emails
-        invalid_domains = ['png', 'jpg', 'gif']
-
-        regex = {
+        self.phone_list = []
+        self.email_list = []
+        self.url_list = []
+        self.regex = {
+            'phone': r"\D(\(?\d{3})\D{0,3}(\d{3})\D{0,3}(\d{4})\D",
             'email': r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",
-            'phone': (r"1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})"
-                      r"(\se?x?t?(\d*))?"),
             'url': (r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]"
                     r"|(?:%[0-9a-fA-F][0-9a-fA-F]))[^\)\"';\s]+")
         }
+
+    def handle_starttag(self, tag, attr):
+        # scrape urls from specific html tags
+        tag_dict = {'link': 'href', 'img': 'src',}
+        if tag in tag_dict.keys():
+            [self.url_list.append(v) for k,v in attr if k == tag_dict[tag]]
+
+        # scrape phone numbers from link tags
+        elif tag == 'a':
+            [self.phone_list.append('({}){}-{}'.format(phone[0], phone[1], phone[2]))
+            for k,v in attr if k == 'href' and re.search(self.regex['phone'], v)]
+                    
+         
+
+    def handle_data(self, data):
+        """ find regex matches and format data for output """
+        regex = self.regex
+        # ensure images do not get scraped as emails
+        invalid_domains = ['png', 'jpg', 'gif']
+
 
         # search raw html for regex matches
         phone_matches = re.findall(regex['phone'], data)
@@ -42,24 +48,31 @@ class HTML_Info_Parser(HTMLParser):
 
         # format lists for output if they exist
         [self.phone_list.append(
-            '({}){}-{}'.format(*format_data(phone[0], phone[1], phone[2])))
-         for phone in format_list(phone_matches) if phone_matches]
+            '({}){}-{}'.format(phone[0], phone[1], phone[2]))
+         for phone in phone_matches if phone_matches]
 
-        [self.email_list.append(*format_data(email))
-         for email in format_list(email_matches) if email_matches and
+        [self.email_list.append(email)
+         for email in email_matches if email_matches and
          email.split('.')[-1] not in invalid_domains]
 
-        [self.url_list.append(*format_data(url))
-         for url in format_list(url_matches) if url_matches]
+        [self.url_list.append(url)
+         for url in url_matches if url_matches]
+
+
+
+def format_list(ls):
+    # sort list and remove duplicates
+    return sorted(list(set(ls)))
 
 
 def scrape_html(url):
     html_req = requests.get(url)
     parser = HTML_Info_Parser()
-    parser.feed(html_req.text)
+    parser.feed(html_req.text.encode('utf8'))
 
-    phones, emails, urls = (parser.phone_list,
-                            parser.email_list, parser.url_list)
+    phones, emails, urls = (format_list(parser.phone_list),
+                            format_list(parser.email_list),
+                            format_list(parser.url_list))
 
     # output
     print('Data found from {}: '.format(url))
